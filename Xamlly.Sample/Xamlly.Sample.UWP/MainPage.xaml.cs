63f551dd2e98f75f87036005df65a72fb298ff9a
Drew Keller <954172+drewkeller@users.noreply.github.com>
2020-09-21 03:12:04 -0500

    * Add UWP project

---
+++ Xamlly.Sample/Xamlly.Sample.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Xamlly.Sample.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new Xamlly.Sample.App());
+        }
+    }
+}
